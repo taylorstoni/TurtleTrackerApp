50ac9ef125e58a9230e7edc5fccdafb8c0651a10
taylorsotni <taylor.stoni@duke.edu>
2020-09-17 17:09:49 -0400

    Initial commit of ARGOS script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Taylor Stoni (taylor.stoni@duke.edu)
+# Date:   Fall 2020
+#--------------------------------------------
+
